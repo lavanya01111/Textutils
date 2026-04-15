6c5bc321c91eec9632984e0bccdcf9bcaeb13da1
lavanya pulani <lavanyapulani@gmail.com>
2026-04-15 10:17:58 +0530

    changes in textform

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -37,15 +37,15 @@ export default function TextForm(props) {
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode ==='dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-        <button className="btn btn-primary mx-2" onClick={handleSentenceClick}>Convert to Sentence case</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleSentenceClick}>Convert to Sentence case</button>
 
     </div>
     <div className="container my-3"style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.split(" ").filter((element) => {return element !==""}).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
